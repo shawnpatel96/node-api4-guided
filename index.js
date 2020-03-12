5a3642c016d42c2968b541521fe368093785c376
shawn <shawnpatel196@gmail.com>
2020-03-12 11:37:13 -0500

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./api/server.js");
-
-const port = 5000;
+//make the port be assigned by the server
+// heroku will place the .PORT environment value on thier server// gave heroku the power to change ports if needed
+const port = process.env.PORT || 5000;  // PORT here has to be uppercase, reserved keyword in linux
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
